826a73c37cdb128f858df5266a5e870c1351ff12
xlp-123 <1004147980@qq.com>
2020-03-19 10:05:24 +0800

    First commmit

---
+++ 3.5.4.py
@@ -0,0 +1,13 @@
+#3.5.4
+import time
+scale=50
+print("strat".center(scale//2,"-"))
+start=time.perf_counter()
+for i in range(scale+1):
+    a="*"*i
+    b="."*(scale-i)
+    c=(i/scale)*100
+    dur=time.perf_counter()-start
+    print("\r{:^3.0f}%[{}->{}]{:.2f}s".format(c,a,b,dur),end="")
+    time.sleep(0.1)
+print("\n"+"end".center(scale//2,"-"))
